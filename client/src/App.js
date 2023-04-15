86807a84d051a5bd66c7eeb42d92eef73c58473b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-04-15 09:27:08 +0300

    initialised the project
    initialised the backend using strapi and frontend using react. Created a theme using mui.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+    
+          Learn React
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
